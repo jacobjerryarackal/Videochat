56671e2af0e40e5db4258225be3d9eaa57065fba
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2024-09-17 13:31:12 +0530

    updated

--- src/app/room/[roomid]/page.tsx
+++ src/app/room/[roomid]/page.tsx
@@ -5,19 +5,19 @@ import { v4 as uuid } from "uuid";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 const Room = ({ params }: { params: { roomid: string } }) => {
-  const { fullName } = useUser(); // Custom hook to get the user's full name
+  const { fullName } = useUser(); 
   const roomID = params.roomid;
   const meetingRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (typeof window !== "undefined" && meetingRef.current) {
-      const appID = parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID!); // Get ZegoCloud App ID from env
-      const serverSecret = process.env.NEXT_PUBLIC_ZEGO_SERVER_SECRET!; // Get ZegoCloud Server Secret from env
+      const appID = parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID!); 
+      const serverSecret = process.env.NEXT_PUBLIC_ZEGO_SERVER_SECRET!; 
 
-      // Prevent multiple room joins by checking if the UI is already rendered
+      
       if (meetingRef.current.childElementCount > 0) return;
 
-      // Generate a kit token for Zego UIKit
+      
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
         appID,
         serverSecret,
@@ -30,7 +30,6 @@ const Room = ({ params }: { params: { roomid: string } }) => {
       const zp = ZegoUIKitPrebuilt.create(kitToken);
 
       try {
-        // Try to join the room
         zp.joinRoom({
           container: meetingRef.current,
           sharedLinks: [
@@ -46,13 +45,13 @@ const Room = ({ params }: { params: { roomid: string } }) => {
             },
           ],
           scenario: {
-            mode: ZegoUIKitPrebuilt.VideoConference, // Using video conference mode
+            mode: ZegoUIKitPrebuilt.VideoConference, 
           },
         });
       } catch (error: any) {
         console.error("Failed to join room:", error);
 
-        // Handle device access errors
+  
         if (error.message.includes("NotReadableError")) {
           alert(
             "Cannot access camera or microphone. Please check your device and browser permissions."
